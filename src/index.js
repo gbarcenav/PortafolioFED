8b06260d0b80c9f44e72270ce309767ddbb0b6c3
gbarcenav <gbarcenav@gmail.com>
2019-11-25 15:22:27 -0600

    Se modificó vista de skills y documento de cv

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const menuHamburguer = document.getElementById("icon-menu");
 const principalMenu = document.getElementById("nav");
+// const containerItemsMenu = document.getElementById("items-menu")
 
 const showMenu = () => {
   principalMenu.classList.toggle("nav-open");
 };
 
 menuHamburguer.addEventListener("click", showMenu);
+
+
+// const hiddeMenu = (e) => {
+//   e.target.style.visibility= "hidden"
+// };
+
+// principalMenu.addEventListener("click", hiddeMenu);
